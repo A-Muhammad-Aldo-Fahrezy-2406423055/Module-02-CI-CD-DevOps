ab1f8e7d69a2a28216773ee10e7e362ae10bfd6e
Muhammad Aldo Fahrezy <muhammadaldofahrezy@Muhammads-MacBook-Pro.local>
2026-02-10 16:03:26 +0700

    Added negative testing scenarios

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.Min;
 
 import java.util.UUID;
 
@@ -9,5 +10,13 @@ import java.util.UUID;
 public class Product {
     private UUID productId;
     private String productName;
+
+    @Min(value=0, message="Quantity must be a positive number")
     private int productQuantity;
+
+    public void setProductQuantity(int productQuantity) {
+        if (productQuantity >= 0) {
+            this.productQuantity = productQuantity;
+        }
+    }
 }
